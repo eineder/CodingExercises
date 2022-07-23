528b50f795cd096a441d655b6fad7b5c2d19a832
max <max.eineder@googlemail.com>
2022-07-23 14:33:05 +0200

    Starting point

--- PasswordValidatorCLI/Program.cs
+++ PasswordValidatorCLI/Program.cs
@@ -1,18 +1,18 @@
 ﻿Console.WriteLine("Hello, this tool allows for validing passwords.");
-const string prompt = "Please enter a password to have it checked or type 'exit' to exit the application.";
+const string prompt = "Please enter a password to have it checked.";
 Console.WriteLine(prompt);
 
 using var stream = Console.OpenStandardInput();
 using var reader = new StreamReader(stream);
 
-string line;
-while ((line = await reader.ReadLineAsync()) != "exit")
-{           
-    Console.WriteLine($"You wrote: {line}");
+var validator = new PasswordValidator();
+
+while (true)
+{
+    var line = await reader.ReadLineAsync();
+
+    // Replace this line with putting out the outcome of the validation.
+    Console.WriteLine($"Your password: {line}"); 
+
     Console.WriteLine(prompt);
 }
-
-
-Console.WriteLine("Exiting...");
-
-
